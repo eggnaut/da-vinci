fb4012ca726b23ef1a0b566749f3ca8ac4c3a2ad
eggnaut <120997069+eggnaut@users.noreply.github.com>
2023-01-12 18:31:00 -0800

    Update __init__.py

--- EggEngine/__init__.py
+++ EggEngine/__init__.py
@@ -1,7 +1,7 @@
 '''
 EggEngine.
 
-A custom framework/engine written with and for Python. Includes functions for various purposes. 
+A custom framework/library written with and for Python. Includes functions for various purposes. 
 Easy to read and use. Docstrings for each function and module.
 
 Made by @eggnaut
