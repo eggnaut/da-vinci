549c4ec52ae33bee33c2f187c3903e770e9788fb
eggnaut <120997069+eggnaut@users.noreply.github.com>
2023-02-03 11:00:12 -0800

    Update __init__.py

--- EggEngine/__init__.py
+++ EggEngine/__init__.py
@@ -1,8 +1,8 @@
 '''
 EggEngine.
 
-A framework/library written with and for Python. Includes functions for various purposes. 
-Easy to read and use. Docstrings for each function and module.
+A framework/library written with and for Python. Includes functions and classes for various purposes. 
+Easy to read and use. Docstrings for each function and module. Open-source and free to use.
 
 Made by @eggnaut
 '''
