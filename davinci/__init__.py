d018b2333a1a6853a6c9c810d9e109e1d8bf0323
ALCUNO <db1519@pleasantonusd.net>
2023-02-12 10:51:12 -0800

    added license info to __init__.py

--- davinci/__init__.py
+++ davinci/__init__.py
@@ -1,8 +1,9 @@
 '''
 da vinci.
 
-A framework/library written with and for Python. Includes functions and classes for various purposes. 
-Easy to read and use. Docstrings for each function and module. Open-source and free to use.
+An all-purpose framework/library written with and for Python. Includes functions and classes for various purposes. 
+Easy to read and use. Docstrings for each function and module. Open-source and free to use. Licensed under 
+GNU Lesser General Public License (v2.1).
 
 Made by @eggnaut
 '''
